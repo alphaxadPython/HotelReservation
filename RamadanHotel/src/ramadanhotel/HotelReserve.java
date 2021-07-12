c16adfbbaa69d4b0d88c32acf2e7574c8ff5c1be
alphaxadPython <alphaxadcoder@gmail.com>
2021-07-12 20:15:11 +0300

    added

--- RamadanHotel/src/ramadanhotel/HotelReserve.java
+++ RamadanHotel/src/ramadanhotel/HotelReserve.java
@@ -6,5 +6,30 @@ public class HotelReserve {
     String name;
     String number;
 
+    public String getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(String hotel) {
+        this.hotel = hotel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    
     
 }
